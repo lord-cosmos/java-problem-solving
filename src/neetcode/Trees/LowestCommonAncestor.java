4a2e8e3270bf1241f4cb645df22a951949acde54
lc <teslaatozinspires7@gmail.com>
2023-02-07 14:07:04 +0530

    add neetcode/LowestCommonAncestor

---
+++ src/neetcode/Trees/LowestCommonAncestor.java
@@ -0,0 +1,18 @@
+package neetcode.Trees;
+
+import neetcode.Trees.TreeNode;
+
+public class LowestCommonAncestor {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    TreeNode curr = root;
+    while (curr != null) {
+      if (p.val > curr.val && q.val > curr.val) {
+        curr = curr.right;
+      } else if (p.val < curr.val && q.val < curr.val) {
+        curr = curr.left;
+      } else {
+        return curr;
+      }
+    }
+  }
+}
