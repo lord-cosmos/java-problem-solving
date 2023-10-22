d6797c178b13d8e93897db9f2cbdce011e4e1ff6
lord-cosmos <teslaatozinspires7@gmail.com>
2023-10-22 18:01:06 -0400

    add dp/HouseRobber

---
+++ src/neetcode/dp/HouseRobber.java
@@ -0,0 +1,19 @@
+package neetcode.dp;
+
+//  Problem Link : https://leetcode.com/problems/house-robber/description/
+
+/**
+ * A class which represents the above problem.
+ */
+public class HouseRobber {
+  public int rob(int[] nums) {
+    int rob1 = 0;
+    int rob2 = 0;
+    for(int n : nums) {
+      int temp = Math.max(rob1 + n, rob2);
+      rob1 = rob2;
+      rob2 = temp;
+    }
+  return rob2;
+  }
+}
